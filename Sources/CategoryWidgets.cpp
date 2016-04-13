6c3aab1e454f57882b2a98a2c9ff356dacab65d1
Aleksey Kontsevich <akontsevich@salford-systems.com>
2016-04-13 13:29:49 +0300

    - Added possibility to create different category widgets

---
+++ Sources/CategoryWidgets.cpp
@@ -0,0 +1,122 @@
+#include <QHBoxLayout>
+
+#include "CategoryWidgets.h"
+#include "widgetbox.h"
+
+/**
+ * @class AbstractCategoryButton is abstract ancestor class for all categories
+ * widgets types
+ * @brief AbstractCategoryButton::AbstractCategoryButton
+ */
+
+AbstractCategory::AbstractCategory(const QString &, QTreeWidget *parent,
+                                   QTreeWidgetItem *item)
+  : QWidget(parent)
+  , mItem(item)
+{
+  mItem->setExpanded(true);
+}
+
+void AbstractCategory::onButtonPress()
+{
+  onPageExpand(!mItem->isExpanded());
+}
+
+void AbstractCategory::onPageExpand(bool expanded)
+{
+  mItem->setExpanded(expanded);
+  int index = mItem->treeWidget()->indexOfTopLevelItem(mItem);
+  ((WidgetBox *)mItem->treeWidget()->parent())->setCurrentIndex(index);
+
+  emit pageExpanded(isExpanded());
+}
+
+/**
+ * @class PageButton
+ * @brief The PageButton class: page (category) button for widget box
+ */
+ButtonCategory::ButtonCategory(const QString &text, QTreeWidget *parent,
+                       QTreeWidgetItem *item)
+  : AbstractCategory(text, parent, item)
+
+{
+  mButton = new QPushButton(text, parent);
+
+  QHBoxLayout *horizontalLayout = new QHBoxLayout(this);
+  horizontalLayout->setSpacing(0);
+  horizontalLayout->setContentsMargins(0, 0, 0, 0);
+  horizontalLayout->addWidget(mButton);    // Add button to layout
+
+  mButton->setIcon(QIcon(":/plugins/widgetbox/expanded.png"));
+  // setFlat(true);
+
+  mButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+  QFontMetrics fm(font());
+  mButton->resize(size().width(), fm.height());
+
+  connect(mButton, SIGNAL(pressed()), this, SIGNAL(buttonPressed()));
+}
+
+void ButtonCategory::setTitle(QString const &title)
+{
+  mButton->setText(title);
+}
+
+void ButtonCategory::onButtonPress()
+{
+  AbstractCategory::onButtonPress();
+
+  if(isExpanded()) {
+    mButton->setIcon(QIcon(":/plugins/widgetbox/expanded.png"));
+  } else {
+    mButton->setIcon(QIcon(":/plugins/widgetbox/collapsed.png"));
+  }
+}
+
+LineCategory::LineCategory(const QString &text, QTreeWidget *parent, QTreeWidgetItem *item)
+  : AbstractCategory(text, parent, item)
+{
+  QHBoxLayout *horizontalLayout = new QHBoxLayout(this);
+  horizontalLayout->setSpacing(4);
+  horizontalLayout->setContentsMargins(0, 0, 6, 0);
+
+  mCheckBox = new QCheckBox(this);
+  horizontalLayout->addWidget(mCheckBox);    // Add checkbox to layout
+
+  QFrame *line1 = new QFrame(this);
+  QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+  sizePolicy.setHorizontalStretch(0);
+  sizePolicy.setVerticalStretch(0);
+  sizePolicy.setHeightForWidth(line1->sizePolicy().hasHeightForWidth());
+  line1->setSizePolicy(sizePolicy);
+  line1->setFrameShadow(QFrame::Plain);
+  line1->setLineWidth(1);
+  line1->setMidLineWidth(1);
+  line1->setFrameShape(QFrame::HLine);
+  horizontalLayout->addWidget(line1);       // Add line1 to layout
+
+  mLabel = new QLabel(text, this);
+  QSizePolicy sizePolicy1(QSizePolicy::Minimum, QSizePolicy::Preferred);
+  sizePolicy1.setHorizontalStretch(0);
+  sizePolicy1.setVerticalStretch(0);
+  sizePolicy1.setHeightForWidth(mLabel->sizePolicy().hasHeightForWidth());
+  mLabel->setSizePolicy(sizePolicy1);
+  horizontalLayout->addWidget(mLabel);      // Add label to layout
+
+  QFrame *line2 = new QFrame(this);
+  sizePolicy.setHeightForWidth(line2->sizePolicy().hasHeightForWidth());
+  line2->setSizePolicy(sizePolicy);
+  line2->setFrameShadow(QFrame::Plain);
+  line2->setLineWidth(1);
+  line2->setMidLineWidth(1);
+  line2->setFrameShape(QFrame::HLine);
+  horizontalLayout->addWidget(line2);       // Add line2 to layout
+
+  setExpanded(false);                       // Expand if checkbox is checked
+  connect(mCheckBox, SIGNAL(toggled(bool)), SLOT(onPageExpand(bool)));
+}
+
+void LineCategory::setTitle(QString const &title)
+{
+  mLabel->setText(title);
+}
---
+++ Sources/CategoryWidgets.h
@@ -0,0 +1,70 @@
+#ifndef CATEGORYWIDGETS_H
+#define CATEGORYWIDGETS_H
+
+#include <QWidget>
+#include <QPushButton>
+#include <QTreeWidget>
+#include <QLabel>
+#include <QCheckBox>
+
+class AbstractCategory : public QWidget
+{
+  Q_OBJECT
+public:
+  AbstractCategory(const QString &text, QTreeWidget *parent, QTreeWidgetItem *item);
+  bool isExpanded() const { return mItem->isExpanded(); }
+  virtual QString title() const = 0;
+
+public slots:
+  virtual void setTitle(QString const &title) = 0;
+  virtual void setExpanded(bool expanded) { mItem->setExpanded(expanded); }
+
+protected:
+  QTreeWidgetItem* item() const { return mItem; }
+
+signals:
+  void buttonPressed();
+  void pageExpanded(bool expanded);
+
+protected slots:
+  virtual void onButtonPress();
+  void onPageExpand(bool expanded = false);
+
+private:
+  QTreeWidgetItem* mItem;
+};
+
+class ButtonCategory : public AbstractCategory
+{
+  Q_OBJECT
+public:
+  ButtonCategory(const QString &text, QTreeWidget *parent, QTreeWidgetItem *item);
+  QString title() const override { return mButton->text(); }
+
+public slots:
+  void setTitle(QString const &title) override;
+
+private slots:
+  void onButtonPress();
+
+private:
+  QPushButton *mButton;
+};
+
+class LineCategory : public AbstractCategory
+{
+  Q_OBJECT
+public:
+  LineCategory(const QString &text, QTreeWidget *parent, QTreeWidgetItem *item);
+  QString title() const override { return mLabel->text(); }
+
+public slots:
+  void setTitle(QString const &title) override;
+  void setExpanded(bool expanded) override { mCheckBox->setChecked(expanded); }
+
+private:
+  QLabel *mLabel;
+  QCheckBox *mCheckBox;
+};
+
+#endif // CATEGORYWIDGETS_H
